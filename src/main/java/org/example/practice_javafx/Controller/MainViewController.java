2dc19ef50a7e1ab7a16b3890c21537e8933748e8
nguyenthieuv1 <nguyennhunhu844@gmail.com>
2024-11-10 09:23:22 +0800

    test commit

--- src/main/java/org/example/practice_javafx/Controller/MainViewController.java
+++ src/main/java/org/example/practice_javafx/Controller/MainViewController.java
@@ -6,7 +6,6 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelReader;
-import javafx.scene.image.WritableImage;
 import javafx.scene.layout.StackPane;
 
 import javafx.stage.FileChooser;
